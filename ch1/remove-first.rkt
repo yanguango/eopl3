34970fd0aba33cddd5f57efa62e2cfeac047396a
yanguango <yanguango@gmail.com>
2015-01-22 14:57:59 -0500

    add remove-first

---
+++ ch1/remove-first.rkt
@@ -0,0 +1,9 @@
+#lang eopl
+(define remove-first
+  (lambda (s los)
+    (if (null? los)
+        '()
+        (if (eqv? (car los) s)
+            (cdr los)
+            (cons (car los) (remove-first s (cdr los)))))))
+
